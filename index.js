808d80324e6f846a16b6c851de5b671eb261b83c
zhaoliang <kybetter@163.com>
2019-08-19 11:34:36 +0800

    refactor: 优化组件名称会带有-的问题

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function scanDir(dir, cache) {
     } else if (/\.vue$/.test(li)) {
       const content = fs.readFileSync(path.resolve(dir, li), 'utf8');
       const regexp = RegExp('@route\(.*\)', 'g');
-
+      let res = null;
+      let matches;
       while ((matches = regexp.exec(content)) !== null) {
         if (matches[0].indexOf(',') > 0) {
           res = /@route\((['"].*\1),\s*(['"].*\2)\)/.exec(matches[0]);
@@ -20,8 +21,8 @@ function scanDir(dir, cache) {
         }
         if (res !== null) {
           const routeStr = res[1].replace(/['"]/g, '');
-          const componentName = routeStr.replace(/[/:?]/g, '');
-          const alias = res[2] ? res[2].replace(/['"']/g, '') : null;
+          const componentName = routeStr.replace(/[/:?*-_]/g, '');
+          const alias = res[2] ? res[2].replace(/['"]/g, '') : null;
           const relatePath = (/.*\/src(\/views.*)/.exec(dir))[1];
 
           cache.push({
@@ -47,16 +48,16 @@ function generatorRoute(routerFile) {
   cache.forEach(item => {
     _imports.push(item.import);
     _routes.push(item.route);
-  })
+  });
   const rawArr = [];
   const length = _imports.length;
   _imports.forEach((item, idx) => {
     rawArr.push(`${item}\n${idx === length - 1 ? '\n' : ''}`);
-  })
+  });
   rawArr.push('export default [\n');
   _routes.forEach(item => {
     rawArr.push(`  {\n    path: '${item.path}',\n    name: '${item.component}',\n    component: ${item.component},\n${item.alias !== null ? `    alias: '${item.alias}',\n`: ''}  },\n`)
-  })
+  });
   rawArr.push('];\n');
   fs.writeFileSync(routerFile, rawArr.join(''));
 }
@@ -66,8 +67,8 @@ class MyPlugin {
     compiler.hooks.afterPlugins.tap('MyPlugin', (compilation, params) => {
       const routerFile = path.resolve('src', 'router', 'children.js');
       generatorRoute(routerFile);
-      console.log('路由文件生成成功')
-      if (process.env.NODE_ENV == 'development') {
+      console.log('路由文件生成成功');
+      if (process.env.NODE_ENV === 'development') {
         const watcher = chokidar.watch(path.resolve('src', 'views'), {
           ignored: /(^|[\/\\])\../,
           persistent: true
@@ -76,7 +77,7 @@ class MyPlugin {
         // TODO：当监听到某个文件变动时，只改动这个文件的路由，而不是再扫描一遍目录
         watcher.on('change', path => {
           generatorRoute(routerFile);
-          console.log('路由文件生成成功')
+          console.log('路由文件生成成功');
         })
       }
     })
